95375282290c8761fc9706efb603bceffb537b40
Yavor Papazov <y_papaz@yahoo.com>
2022-10-14 11:32:16 -0500

    add initial code

---
+++ myApp.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+	let para = document.querySelector('.h3')
+	let a = [1, 2]
+	let b = 2 ** 3
+	console.log(a, b)
+	para.textContent = 'Hello, friends'
+})

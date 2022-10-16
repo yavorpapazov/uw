c5272c1ead2438c61eea6d1621b10b525e8f1fe5
Yavor Papazov <y_papaz@yahoo.com>
2022-10-16 11:55:58 -0500

    new feature complete

--- myApp.js
+++ myApp.js
@@ -2,5 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	let para = document.querySelector('.h3')
 	let d = new Date(2022, 5, 15)
 	console.log(d)
-	para.textContent = 'Hello, friends! This is a complete change!'
+	let c = 4 + 5
+	para.textContent = `Hello, friends! This is a complete change! The result is ${c}`
 })

81b72988f765ad71340cff4dcf1c39e01c3765f5
Yavor Papazov <y_papaz@yahoo.com>
2022-10-16 10:30:21 -0500

    add Date

--- myApp.js
+++ myApp.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 	let para = document.querySelector('.h3')
-	let a = [1, 2]
-	let b = 2 ** 3
-	console.log(a, b)
+	let d = new Date(2022, 5, 15)
+	console.log(d)
 	para.textContent = 'Hello, friends! This is a complete change!'
 })

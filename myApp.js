89385e37ebaea962f3830d1182e3af2488121cb4
Yavor Papazov <y_papaz@yahoo.com>
2022-10-18 10:52:08 -0500

    add a function

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 	let para = document.querySelector('.h3')
-	let d = new Date(2022, 5, 15)
-	console.log(d)
-	let c = 4 + 5
-	para.textContent = `Hello, friends! This is a complete change! The result is ${c}`
+	function addNum(n) {
+		let local = n
+		return local
+	}
+	console.log(addNum(5))
+	para.textContent = `Hello, friends!`
 })
